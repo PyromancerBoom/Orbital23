8edc3d8fc3d88dc9aa2b0e17ea29bf29c873f8d2
PyromancerBoom <priyanshmagic@gmail.com>
2023-06-09 19:19:14 +0530

    tested and working code with port specification in kitex

--- api-gateway/hertz_server/biz/handler/asset_api/asset_api.go
+++ api-gateway/hertz_server/biz/handler/asset_api/asset_api.go
@@ -5,19 +5,14 @@ import (
 	"fmt"
 
 	asset_api "api-gateway/hertz_server/biz/model/asset_api"
+	"api-gateway/hertz_server/kitex_gen/asset_management"
+	"api-gateway/hertz_server/kitex_gen/asset_management/assetmanagement"
 
-	"github.com/cloudwego/kitex/client/genericclient"
-	"github.com/cloudwego/kitex/pkg/generic"
+	clientK "github.com/cloudwego/kitex/client"
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/protocol/consts"
 )
-
-// Generic client initialisation
-func NewGenericClient(destServiceName string) genericclient.Client {
-	genericCli := genericclient.NewClient(destServiceName, generic.BinaryThriftGeneric())
-	return genericCli
-}
 
 // QueryStudent .
 // @router asset/query [GET]
@@ -30,101 +25,84 @@ func QueryAsset(ctx context.Context, c *app.RequestContext) {
 		return
 	}
 
-	// Use generic client
-	// genericclient.NewClient in func NewGenericClient handles all errors
-	client := NewGenericClient()
+	//add other code here:
+	fmt.Println("Recieved Query Request")
 
-	// The resulting reqRpc object represents the request to be
-	// sent to the "QueryAsset" method, with the
-	// asset ID provided as a string value.
-	// It allows the client to specify which asset it wants
-	// to query in the remote "asset" service.
-	requestRpc := &QueryAssetRequest{
-		ID: fmt.Sprintf("%d", req.ID),
-	}
-
-	// Responsible for making an RPC Call
-	// to the "QueryAsset" method of the "asset" service
-	// using the Kitex client.
-	responseRpc, err := client.QueryAsset(ctx, requestRpc)
+	//Use Kitex lient to make rpc call
+	client, err := assetmanagement.NewClient("asset", clientK.WithHostPorts("127.0.0.1:8888"))
 	if err != nil {
 		panic(err)
 	}
 
-	if !responseRpc.Exist {
+	reqRpc := &asset_management.QueryAssetRequest{
+		ID: fmt.Sprintf("%d", req.ID),
+	}
+
+	respRpc, err := client.QueryAsset(ctx, reqRpc)
+	if err != nil {
+		panic(err)
+	}
+
+	//RPC call made
+
+	if !respRpc.Exist {
 		resp := &asset_api.QueryAssetResponse{
 			Msg: fmt.Sprintf("No data for asset ID: %d", req.ID),
 		}
-		// The response object (resp) is serialized as JSON
-		// using c.JSON(200, resp) and sent as the HTTP response.
-		c.JSON(consts.StatusOK, resp)
+		c.JSON(200, resp)
 		return
 	}
 
-	// Constructs response for client
-	// A new instance of asset_api.QueryAssetResponse is
-	// created, and its fields ID, Name, and Market are
-	// assigned values from the corresponding fields of
-	// the respRpc response object.
 	resp := &asset_api.QueryAssetResponse{
-		ID:     responseRpc.ID.(int),
-		Name:   responseRpc.Name.(string),
-		Market: responseRpc.Market.(string),
+		ID:     respRpc.ID,
+		Name:   respRpc.Name,
+		Market: respRpc.Market,
 	}
 
 	c.JSON(consts.StatusOK, resp)
 }
 
-// InsertAsset is an HTTP handler function for handling the "asset/insert" route with a POST method.
-// It receives an asset_api.InsertAssetRequest object from the client, validates the request,
-// and performs an RPC call to the "InsertAsset" method of the "asset" service using a generic client.
-// The response from the RPC call is converted into an asset_api.InsertAssetResponse object
-// and sent back to the client as an HTTP response.
+// InsertAsset
+// @router asset/insert [POST]
 func InsertAsset(ctx context.Context, c *app.RequestContext) {
-	// Initialize variables
 	var err error
 	var req asset_api.InsertAssetRequest
-
-	// Bind and validate the request data received from the client
 	err = c.BindAndValidate(&req)
 	if err != nil {
 		c.String(400, err.Error())
 		return
 	}
 
-	fmt.Println("Received Insert Request")
+	fmt.Println("Recieved Insert Request")
 
-	// Use a generic client to make the RPC call
-	client := NewGenericClient("asset")
+	client, err := assetmanagement.NewClient("asset", clientK.WithHostPorts("127.0.0.1:8888"))
+	if err != nil {
+		panic(err)
+	}
 
-	// Create the request object to be sent to the "InsertAsset" method
-	requestRpc := &InsertAssetRequest{
+	reqRpc := &asset_management.InsertAssetRequest{
 		ID:     req.ID,
 		Name:   req.Name,
 		Market: req.Market,
 	}
 
-	// Make an RPC call to the "InsertAsset" method using the generic client
-	responseRpc, err := client.InsertAsset(ctx, requestRpc)
+	respRpc, err := client.InsertAsset(ctx, reqRpc)
 	if err != nil {
 		panic(err)
 	}
 
-	// Check if the RPC call was successful
-	if !responseRpc.Ok {
-		// If the response indicates an error, construct the error response
+	if !respRpc.Ok {
 		resp := asset_api.InsertAssetResponse{
 			OK:  false,
-			Msg: responseRpc.Msg.(string),
+			Msg: respRpc.Msg,
 		}
 		c.JSON(200, resp)
 		return
 	}
 
-	// Construct the successful response
 	resp := asset_api.InsertAssetResponse{
 		OK:  true,
-		Msg: responseRpc.Msg.(string),
+		Msg: respRpc.Msg,
 	}
 
 	c.JSON(200, resp)
--- api-gateway/kitex_server/main.go
+++ api-gateway/kitex_server/main.go
@@ -1,16 +1,48 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"net"
 	asset_management "api-gateway/kitex_server/kitex_gen/asset_management/assetmanagement"
+	"os"
+
+	server "github.com/cloudwego/kitex/server"
 )
 
 func main() {
-	svr := asset_management.NewServer(new(AssetManagementImpl))
+
+	port := "127.0.0.1:" + getPort()
+	addr, _ := net.ResolveTCPAddr("tcp", port)
+	svr := asset_management.NewServer(new(AssetManagementImpl), server.WithServiceAddr(addr))
 
 	err := svr.Run()
 
 	if err != nil {
 		log.Println(err.Error())
 	}
+}
+
+// returns the port retrieved from cmd, if input is "", then from port.config file. If that fails, returns "8080" /default port.
+func getPort() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Please enter port: ")
+
+	scanner.Scan()
+	input := scanner.Text()
+
+	//read the .config file if no port is mentioned
+	if len(input) == 0 {
+		fmt.Println("Reading port.config for port")
+		data, err := ioutil.ReadFile("port.config")
+		if err != nil {
+			fmt.Println("File reading error. Using port 8080", err)
+			return "8080" //default port
+		}
+		return string(data)
+	}
+
+	return string(input)
 }
