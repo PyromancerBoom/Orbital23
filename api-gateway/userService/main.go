5065696f773cb93526b47cb0ae59fa2de4d5cf4e
PyromancerBoom <priyanshmagic@gmail.com>
2023-06-16 15:32:18 +0530

    kitex server working

--- api-gateway/userService/main.go
+++ api-gateway/userService/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	userservice "api-gateway/userService/kitex_gen/UserService/userservice"
 	"log"
+	"net"
+
+	"github.com/cloudwego/kitex/server"
 )
 
 func main() {
-	svr := userservice.NewServer(new(UserServiceImpl))
+	port := "127.0.0.1:8888"
+	addr, _ := net.ResolveTCPAddr("tcp", port)
+	svr := userservice.NewServer(new(UserServiceImpl), server.WithServiceAddr(addr))
 
 	err := svr.Run()
 
