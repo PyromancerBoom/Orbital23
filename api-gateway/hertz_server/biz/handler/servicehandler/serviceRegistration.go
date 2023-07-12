2b0bb869a209d5048f281208450c6d59da3998c9
PyromancerBoom <priyanshmagic@gmail.com>
2023-07-12 20:26:11 +0530

    update register endpoint

--- api-gateway/hertz_server/biz/handler/servicehandler/serviceRegistration.go
+++ api-gateway/hertz_server/biz/handler/servicehandler/serviceRegistration.go
@@ -1,101 +1,70 @@
 package servicehandler
 
-// import (
-// 	"context"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
 
-// 	"github.com/cloudwego/hertz/pkg/app"
-// 	"github.com/cloudwego/hertz/pkg/protocol/consts"
-// 	// "bytes"
-// 	// "context"
-// 	// "encoding/json"
-// 	// "api-gateway/hertz_server/biz/model/repository"
-// 	// "github.com/cloudwego/hertz/pkg/app"
-// 	// "github.com/cloudwego/hertz/pkg/protocol/consts"
-// 	// "github.com/google/uuid"
-// )
+	repository "api-gateway/hertz_server/biz/model/repository"
 
-// // var db *repository.Database
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+	"github.com/google/uuid"
+)
 
-// // func Init() error {
-// // 	var err error
-// // 	db, err = repository.ConnectDB("http://localhost:27017/", "testDB", "testCollection")
-// // 	if err != nil {
-// // 		return err
-// // 	}
-// // 	return nil
-// // }
+// Handler for /register
+// Registers info sent through a post request
+// Generates API key for admin
+func Register(ctx context.Context, c *app.RequestContext) {
+	var req []map[string]interface{}
 
-// // func Close() {
-// // 	if db != nil {
-// // 		db.Close()
-// // 	}
-// // }
+	// Getting the request Body
+	reqBody, err := c.Body()
+	if err != nil {
+		c.String(consts.StatusBadRequest, "Request body is missing")
+		return
+	}
+	buf := bytes.NewBuffer(reqBody)
 
-// func Register(ctx context.Context, c *app.RequestContext) {
-// 	// var req []map[string]interface{}
+	// Decode the JSON request
+	err = json.NewDecoder(buf).Decode(&req)
+	if err != nil {
+		c.String(consts.StatusBadRequest, "Failed to parse request body")
+		return
+	}
 
-// 	// // Getting the request Body
-// 	// reqBody, err := c.Body()
-// 	// if err != nil {
-// 	// 	c.String(consts.StatusBadRequest, "Request body is missing")
-// 	// 	return
-// 	// }
-// 	// buf := bytes.NewBuffer(reqBody)
+	apiKey := uuid.New().String()
 
-// 	// // Decode the JSON request
-// 	// err = json.NewDecoder(buf).Decode(&req)
-// 	// if err != nil {
-// 	// 	c.String(consts.StatusBadRequest, "Failed to parse request body")
-// 	// 	return
-// 	// }
+	// Store the admin info in the database
+	adminConfig := repository.AdminConfig{
+		ApiKey:    apiKey,
+		OwnerName: req[0]["OwnerName"].(string),
+		OwnerId:   req[0]["OwnerId"].(string),
+		Services:  []repository.Service{},
+	}
 
-// 	// apiKey := uuid.New().String()
+	err = repository.StoreAdminInfo(adminConfig)
+	if err != nil {
+		c.String(consts.StatusInternalServerError, "Failed to register admin")
+		return
+	}
 
-// 	// // Register information in Mongo DB along with the apiKey
-// 	// for _, data := range req {
-// 	// 	ownerId := data["ownerId"].(string)
-// 	// 	if isAlreadyRegistered(ownerId) {
-// 	// 		c.String(consts.StatusBadRequest, "Owner ID already exists")
-// 	// 		return
-// 	// 	}
-// 	// 	clientData := &repository.ClientData{
-// 	// 		OwnerId: ownerId,
-// 	// 		ApiKey:  apiKey,
-// 	// 	}
+	response := make(map[string]string)
+	response["Message"] = "Registered successfully. You're good to GO :D"
+	response["api-key"] = apiKey
 
-// 	// 	// Use the db instance to call the StoreClientData method
-// 	// 	err := db.StoreClientData(clientData)
-// 	// 	if err != nil {
-// 	// 		c.String(consts.StatusInternalServerError, "Failed to store client data")
-// 	// 		return
-// 	// 	}
-// 	// }
+	c.JSON(consts.StatusOK, response)
+}
 
-// 	// res := make(map[string]string)
-// 	// res["Message"] = "Registered successfully. You're good to GO :D"
-// 	// res["api-key"] = apiKey
-
-// 	// c.JSON(consts.StatusOK, res)
-
-// 	c.JSON(consts.StatusOK, "res")
-// }
-
-// // Function to check if owner ID already exists using the ownerIds map
-// func isAlreadyRegistered(ownerId string) bool {
-// 	// clientData, err := db.GetClientDataByOwnerID(ownerId)
-// 	// if err != nil {
-// 	// 	return false
-// 	// }
-// 	// return clientData != nil
-// }
-
-// func DisplayAll(ctx context.Context, c *app.RequestContext) {
-// 	// clientDataList, err := db.GetAllClientData()
-// 	// if err != nil {
-// 	// 	c.String(consts.StatusInternalServerError, "Failed to fetch client data")
-// 	// 	return
-// 	// }
-
-// 	// c.JSON(consts.StatusOK, clientDataList)
-// 	c.JSON(consts.StatusOK, "res")
-// }
+// Utility Function to check if ownerID is already registered in the database
+// @Params:
+// - ownerId: string - The owner ID to check
+// @Returns:
+// - bool: true if already registered, false otherwise
+func isAlreadyRegistered(ownerId string) bool {
+	_, err := repository.GetAdminInfoByID(ownerId)
+	if err != nil {
+		return false
+	}
+	return true
+}
