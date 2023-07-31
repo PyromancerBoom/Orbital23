0178e424d0345ef2c3cd5cd2380c9f31ba626a57
PyromancerBoom <priyanshmagic@gmail.com>
2023-07-31 18:49:08 +0530

    update unit test for server utils

--- api-gateway/server_utils/server_utils.go
+++ api-gateway/server_utils/server_utils.go
@@ -1,4 +1,4 @@
-package main
+package server_utils
 
 /*
 Exported Methods in this package :
@@ -45,6 +45,10 @@ type ConnectRequest struct {
 }
 
 func NewGatewayClient(apikey string, serviceName string, gatewayAddress string) *GatewayClient {
+	if apikey == "" || serviceName == "" || gatewayAddress == "" {
+		log.Println("Error creating GatewayClient. API Key, Service Name and Gateway Address cannot be blank")
+		return nil
+	}
 	return &GatewayClient{
 		ApiKey:         apikey,
 		ServiceName:    serviceName,
